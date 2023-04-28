3c6a1286b0fd8bf59227a66607f6fe8bc2dbca59
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2023-04-29 04:36:14 +0800

    Update

--- js/nav_script.js
+++ js/nav_script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const nav = document.querySelector('.navbar')
 fetch('/nav.html')
 .then(res=>res.text())
@@ -8,14 +7,3 @@ fetch('/nav.html')
     const doc = parser.parseFromString(data, 'text/html')
     eval(doc.querySelector('nav_script').textContent)
 })
-=======
-const nav = document.querySelector('.navbar')
-fetch('/nav.html')
-.then(res=>res.text())
-.then(data=>{
-    nav.innerHTML=data
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(data, 'text/html')
-    eval(doc.querySelector('nav_script').textContent)
-})
->>>>>>> new-branch
