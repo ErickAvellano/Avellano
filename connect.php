f126b462418c9afc58a8e462a6aa83a026533cc3
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2023-05-26 01:07:34 +0800

    sql php

--- connect.php
+++ connect.php
@@ -1,19 +1,14 @@
 <?php
-if (!empty($_POST)) {
-  $name = isset($_POST['name']) ? $_POST['name'] : '';
-  $email = isset($_POST['email']) ? $_POST['email'] : '';
-  $message = isset($_POST['message']) ? $_POST['message'] : '';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "websystem_db";
 
-$conn = new mysqli('localhost','root','','websystem_db');
-if($conn->connect_error){
-  die('Connection Failed: ' . $conn->connect_error);
-}else{
-  $stmt = $conn->prepare("insert into contact (Name, Email, Message) VALUES (?, ?, ?)");
-  $stmt->bind_param("sss", $name, $email, $message);
-  $stmt->execute();
-  echo "Registration Successful";
-  $stmt->close();
-  $conn->close();
-}
+// Create a new connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
