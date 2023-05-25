99d9ca9fc727af680b8e9d1166cfef625f4d111d
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2023-05-25 11:43:42 +0800

    php

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+  
+    $con = new mysqli('localhost', 'root', '', 'websystem_db');
+    if ($con->connect_error) {
+      die('Connection Failed: '.$con->connect_error);
+    } else {
+      $stmt = $con->prepare("INSERT INTO contact (name, email, message) VALUES (?, ?, ?)");
+      $stmt->bind_param("sss", $name, $email, $message);
+      $stmt->execute();
+      echo "Message Sent";
+      $stmt->close();
+      $con->close();
+    }
+}
+?>
