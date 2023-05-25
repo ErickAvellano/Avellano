0392fc782a8d22e22b2f34dd08ea935d24fd2035
ErickAvellano <erickdevega.avellano@bicol-u.edu.ph>
2023-05-26 01:07:03 +0800

    Sql and Php

---
+++ saveFormData.php
@@ -0,0 +1,34 @@
+<?php
+include 'connect.php'; // Include the database connection file
+
+var_dump($_POST);
+
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+} else {
+    $name = "";
+}
+
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+} else {
+    $email = "";
+}
+
+if (isset($_POST['message'])) {
+    $message = $_POST['message'];
+} else {
+    $message = "";
+}
+
+$sql = "INSERT INTO contact (Name, Email, Message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form data inserted successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
